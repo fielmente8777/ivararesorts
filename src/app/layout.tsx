42ec9079c10da46555b4784619a5bbf32a6ac22d
Personal <sk7627577@gmail.com>
2026-07-17 11:23:45 +0530

    up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,8 +53,33 @@ export default function RootLayout({
           href="/favicon/favicon-16x16.png"
         />
         <link rel="manifest" href="/favicon/site.webmanifest"></link>
+
+        {/* <!-- Google Tag Manager --> */}
+        <Script
+          id="gtm"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PFSF6H54');`,
+          }}
+        ></Script>
+        {/* <!-- End Google Tag Manager --> */}
       </head>
       <body className="min-h-full flex flex-col">
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PFSF6H54"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
         {children}
         <LandingFooter />
         <Whatsapp whatsAppNumber={contact.phone[0]} />
